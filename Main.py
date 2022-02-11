ffebba2b043785ee89ab2ca7fdd7fcd835463608
michaeljhr <99460971+michaeljhr@users.noreply.github.com>
2022-02-11 13:51:03 +0800

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,51 @@
+from nltk import tokenize
+from operator import itemgetter
+import math
+
+doc = input("Text: ")
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+stop_words = set(stopwords.words('english'))
+
+
+total_words = doc.split()
+total_word_length = len(total_words)
+print(total_word_length)
+
+total_sentences = tokenize.sent_tokenize(doc)
+total_sent_len = len(total_sentences)
+print(total_sent_len)
+
+tf_score = {}
+for each_word in total_words:
+    each_word = each_word.replace('.','')
+    if each_word not in stop_words:
+        if each_word in tf_score:
+            tf_score[each_word] += 1
+        else:
+            tf_score[each_word] = 1
+
+# Dividing by total_word_length for each dictionary element
+tf_score.update((x, y/int(total_word_length)) for x, y in tf_score.items())
+print(tf_score)
+
+
+def check_sent(word, sentences):
+    final = [all([w in x for w in word]) for x in sentences]
+    sent_len = [sentences[i] for i in range(0, len(final)) if final[i]]
+    return int(len(sent_len))
+
+
+tf_idf_score = {key: tf_score[key] * tf_score.get(key, 0) for key in tf_score.keys()}
+print(tf_idf_score)
+
+
+def get_top_n(dict_elem, n):
+    result = dict(sorted(dict_elem.items(), key = itemgetter(1), reverse = True)[:n])
+    return result
+
+
+print(get_top_n(tf_idf_score, 5))
